71e73f11aaf5c91b1c84f16404b1e6952c5b10e4
eyoson <eyoson@waifu.club>
2019-11-30 21:43:51 -0800

    added score in comment

--- 2019/01.rb
+++ 2019/01.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
-nums = File.open(ARGV.first).readlines.map &:to_i
+nums = File.readlines(ARGV.first).map &:to_i
 
 def p1(x)
-  (x / 3).floor - 2
+  x / 3 - 2
 end
 
 def p2(x)
@@ -18,5 +18,9 @@ def p2(x)
   ret
 end
 
-puts lines.map{ |x| p1 x }.sum
-puts lines.map{ |x| p2 x }.sum
+puts nums.map{ |x| p1 x }.sum
+puts nums.map{ |x| p2 x }.sum
+
+#       -------Part 1--------   -------Part 2--------
+# Day       Time  Rank  Score       Time  Rank  Score
+#   1   00:01:59   238      0   00:07:40   414      0
