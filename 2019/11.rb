e9a9237ba5ae7754b3b690cda6c37837f19f3afc
eyoson <eyoson@waifu.club>
2019-12-11 20:37:50 -0800

    cleaner day 11

--- 2019/11.rb
+++ 2019/11.rb
@@ -13,7 +13,7 @@ class Ord
   West  = 3
   MAX   = 4
 
-  def initialize(x: ORD::North)
+  def initialize(x: North)
     @val = x
   end
 
@@ -24,133 +24,80 @@ class Ord
 
 end
 
-class Pos < Array
-  def initialize(val: [0,0])
-    @val = val
-  end
-
+class Array
   def move(dir)
-    case dir
-    when ORD::North then self[0] -= 1
-    when ORD::East  then self[1] += 1
-    when ORD::South then self[0] += 1
-    when ORD::West  then self[1] -= 1
+    case dir.val
+    when Ord::North then self[0] -= 1
+    when Ord::East  then self[1] += 1
+    when Ord::South then self[0] += 1
+    when Ord::West  then self[1] -= 1
     end
   end
 end
 
+def paint(tape, black: Set.new, white: Set.new)
+  dir   = Ord.new
+  pos   = [0, 0]
+  paint = true
 
-class Turtle
-  def initialize(tape)
-    @tape = tape
-  end
+  machine = Interpreter.new(tape)
+  until machine.done
+    machine.input[0] = white.include?(pos) ? 1 : 0
+    machine.opcycle
 
-  def draw(black: Set.new, white: Set.new)
-    dir = Ord.new
-    pos = Pos.new
+    next if machine.output.empty?
 
-    until machine.done
-      machine.input[0] = white.include?(pos) ? 1 : 0
-      machine.opcycle
-
-      next if machine.output.empty?
-
-      out = machine.output.pop
-      if i.zero?
-        if out.zero?
-          black << pos.dup
-          white.delete(pos)
-        else
-          white << pos.dup
-          black.delete(pos)
-        end
+    out = machine.output.pop
+    if paint
+      if out.zero?
+        black << pos.dup
+        white.delete(pos)
       else
-        direction.rot(out)
-
-        case direction.val
-        when 0 then pos[0] -= 1
-        when 1 then pos[1] += 1
-        when 2 then pos[0] += 1
-        when 3 then pos[1] -= 1
-        end
+        white << pos.dup
+        black.delete(pos)
       end
-
-      i += 1
-      i %= 2
+    else
+      dir.rot(out)
+      pos.move(dir)
     end
 
-    [white, black]
+    paint ^= true
   end
+
+  [white, black]
 end
 
+def paint_canvas(white, black)
+  min_y, max_y = (white + black).map(&:first).minmax
+  min_x, max_x = (white + black).map(&:last).minmax
 
-def move(dir, arr)
+  paint_line =
+    lambda (line, y) do
+      line.map
+          .with_index { |c, x| 
+            y -= min_y
+            x -= min_x
+            white.include?([y, x]) ? '██' : '░░'
+          }
+          .join
+    end
+
+  canvas = Array.new(max_y + 1, Array.new(max_x + 1))
+  canvas.map
+        .with_index(&paint_line)
+        .join("\n")
 end
 
 tape = File.read(ARGV.first)
            .split(',')
            .map(&:to_i)
 
-direction = Ord.new(0)
-pos       = [0, 0]
-machine   = Interpreter.new(tape)
-black     = Set.new
-white     = Set.new
-white << [0, 0]
-
-i = 0
-until machine.done
-  machine.input[0] = white.include?(pos) ? 1 : 0
-  machine.opcycle
-
-  next if machine.output.empty?
-
-  out = machine.output.pop
-  if i.zero?
-    if out.zero?
-      black << pos.dup
-      white.delete(pos)
-    else
-      white << pos.dup
-      black.delete(pos)
-    end
-  else
-    direction.rot(out)
-
-    case direction.val
-    when 0 then pos[0] -= 1
-    when 1 then pos[1] += 1
-    when 2 then pos[0] += 1
-    when 3 then pos[1] -= 1
-    end
-  end
-
-  i += 1
-  i %= 2
-end
-
-union = white | black
-silver = union.size
-
-grid = Array.new(30) { Array.new(30) }
-
-union.each { |pos|
-  y, x = pos
-  color = (white.include?(pos) ? '#' : ' ')
-  grid[y][x - 1] = color
-}
-
-grid.map!(&:compact)
-grid.reject!(&:empty?)
-
-
-grid.each { |x| puts x.join('') }
-
+silver = paint(tape).reduce(:|).size
+gold   = paint_canvas(*paint(tape, :white => Set.new([[0, 0]])))
 
 puts(
-  "Day 11\n"   \
-  "======\n"   \
+  "Day 11\n"       \
+  "======\n"       \
   "✮: #{silver}\n" \
-  "★: #{gold}"
+  "★: \n#{gold}"
 )
-
