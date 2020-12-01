b6da44ff8eadbff22cfa5fb3697d96c7f4c3d1bc
eyoson <eyoson@waifu.club>
2020-12-01 01:49:40 -0800

    DUDE 504 GATEWAY LMAO

--- 2020/01.rb
+++ 2020/01.rb
@@ -3,14 +3,25 @@
 
 require "set"
 
-input = File.open("input/01.txt").read.strip
+NUMS = File.readlines("input/01.txt")
+  .map(&:to_i)
+  .to_set
 
-silver = input.split("").map { |x| x }.reduce
-gold = nil
+def solve(year = 2020)
+  ret = NUMS.find { NUMS.member? year - _1 }
+
+  ret ? ret * (year - ret) : nil
+end
+
+silver = solve
+gold = NUMS.map {
+  tmp = solve(2020 - _1)
+  tmp ? tmp * _1 : nil
+}.find(&:itself)
 
 puts(
   "Day 01\n" \
-  "======\n" \
+  "==================\n" \
   "✮: #{silver}\n" \
   "★: #{gold}"
 )
