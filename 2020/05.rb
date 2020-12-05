04ad10cf371109ae5e86a75eecd32f2fdfed9cac
eyoson <eyoson@waifu.club>
2020-12-05 01:48:45 -0800

    05 fast version

--- 2020/05.rb
+++ 2020/05.rb
@@ -1,24 +1,10 @@
-require "set"
+#!/usr/bin/env ruby -w
+# frozen_string_literal: true
 
-I = File.readlines("input/05.txt", chomp: true)
+seats = File.readlines("input/05.txt").map { _1.tr("FBLR", "0101").to_i 2 }.sort
 
-def binary_search(upper, lower, ticket, &fn)
-  ticket.each_char do |c|
-    mid = (upper + lower) / 2
-    fn.call(c) ? lower = mid : upper = mid
-  end
-
-  lower
-end
-
-def get_seat(ticket)
-  binary_search(0, 127, ticket[0..6]) { _1 == "F" } * 8 +
-    binary_search(0, 7, ticket[7..]) { _1 == "L" }
-end
-
-seats = I.map { get_seat _1 }.to_set
 silver = seats.max
-gold = seats.find { !seats.include?(_1 + 1) && seats.include?(_1 + 2) } + 1
+gold = seats.each_with_index.find { |x, i| x + 2 == seats[i + 1] }.first + 1
 
 puts(
   "Day 05\n" \
