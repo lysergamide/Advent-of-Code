4eacca8c05f0f2b010e25ff99ef134b62baef4bb
eyoson <eyoson@waifu.club>
2020-12-06 03:31:40 -0800

    golfing

--- 2020/06.rb
+++ 2020/06.rb
@@ -1,7 +1,10 @@
 #!/usr/bin/env ruby -w
 # frozen_string_literal: true
 
-groups = File.read("input/06.txt").split("\n\n").map { _1.split.map &:chars }
+groups = File
+  .read("input/06.txt")
+  .split("\n\n")
+  .map { _1.split.map &:chars }
 
 silver = groups.sum { _1.reduce(:|).size }
 gold = groups.sum { _1.reduce(:&).size }
@@ -13,5 +16,5 @@ puts(
   "★: #{gold}"
 )
 
-# golf
-# p $<.read.split("\n\n").map { _1.split.map &:chars }.then { |l| [:|, :&].map { |f| l.sum{ _1.reduce(f).size } } }
+# golf 90 characters
+# $/="\n\n";p$<.map{_1.split.map &:chars}.then{|l|[:|,:&].map{|f|l.sum{ _1.reduce(f).size}}}
