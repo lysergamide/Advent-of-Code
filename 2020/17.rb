f2f07a1ae11e398ce36eb37ca613bd9308adc95e
eyoson <eyoson@waifu.club>
2020-12-20 20:57:46 -0800

    17, 18

---
+++ 2020/17.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby -w
+# frozen_string_literal: true
+
+require "set"
+I = File.readlines("input/17.txt")
+
+def neighbors(p) (p.map { (_1 - 1.._1 + 1).to_a }.then { |a, *b| a.product(*b) }) - [p] end
+
+def solve(p2 = false)
+  alive = I.each_index
+    .flat_map { |i| (0...I[0].size).map { |j| p2 ? [i, j, 0, 0] : [i, j, 0] if I[i][j] == "#" } }
+    .compact
+    .to_set
+
+  6.times do
+    alive =
+      Hash.new { |h, k| h[k] = 0 }
+        .tap { |h| alive.each { |cube| neighbors(cube).each { h[_1] += 1 } } }
+        .then { |h| h.each_key.filter { h[_1] == 3 || (alive.include?(_1) && h[_1] == 2) } }
+        .to_set
+  end
+
+  alive.size
+end
+
+puts("Day 17\n" \
+"==================\n" \
+"✮: #{solve}\n" \
+"★: #{solve true}")
---
+++ 2020/18.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby -w
+# frozen_string_literal: true
+
+I = File.readlines("input/18.txt")
+
+O = {
+  "+" => ->(a, b) { (a + b).to_s },
+  "*" => ->(a, b) { (a * b).to_s },
+}.freeze
+
+# dudge regex lmao
+def solve(line, p2 = false)
+  while /(?<outer>\((?<inner>[^()]*)\))/ =~ line
+    line.sub!(outer, solve(inner, p2))
+  end
+
+  if p2
+    while /(?<outer>(?<a>\d+) (?<o>\+) (?<b>\d+))/ =~ line
+      line.sub!(outer, O[o].call(a.to_i, b.to_i))
+    end
+
+    while /(?<outer>(?<a>\d+) (?<o>\*) (?<b>\d+))/ =~ line
+      line.sub!(outer, O[o].call(a.to_i, b.to_i))
+    end
+  else
+    while /(?<outer>(?<a>\d+) (?<o>\+|\*) (?<b>\d+))/ =~ line
+      line.sub!(outer, O[o].call(a.to_i, b.to_i))
+    end
+  end
+
+  line
+end
+
+puts("Day 18\n" \
+"==================\n" \
+"✮: #{I.sum { solve(_1.clone).to_i }}\n" \
+"★: #{I.sum { solve(_1, true).to_i }}")
