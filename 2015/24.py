ac0a057827be5db84994cf1799c8bc50181de628
eyoson <eyoson@waifu.club>
2021-06-22 00:20:47 -0700

    formatting

---
+++ 2015/24.py
@@ -0,0 +1,12 @@
+import itertools, operator, functools
+# AoC 2015 Makes Me Sad
+def Solve(Groups = 3, Start = 2):
+    Ideal = sum(Input) // Groups
+    while sum(Input[:Start]) < Ideal:
+        Start += 1
+    for N in range(Start, len(Input) - Groups):
+        if List := [functools.reduce(operator.mul, x, 1) for x in itertools
+            .combinations(Input, N) if sum(x) == Ideal]: return min(List)
+# Why is it so freaking simple
+Input = sorted(map(int, open('input/24.txt')), reverse
+    = True); print("Silver:", Solve(), "\nGold:", Solve(4))
