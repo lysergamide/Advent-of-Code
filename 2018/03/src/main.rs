f309c3c2a5858a455f3b8a7d2b351549b0e76f5a
eyoson <eyoson@waifu.club>
2021-06-29 20:25:09 -0700

    starting 03 in rust

--- 2018/03/src/main.rs
+++ 2018/03/src/main.rs
@@ -1,3 +1,37 @@
+extern crate regex;
+
+use regex::Regex;
+use std::collections::HashMap;
+use std::str::FromStr;
+
+const I: &str = include_str!("../03.txt");
+
+struct fabric {
+    id: i32,
+    top_left: [i32; 2],
+    bottom_right: [i32; 2],
+}
+
 fn main() {
-    println!("Hello, world!");
+    //    let mut map = HashMap::new();
+
+    let re = Regex::new(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)").unwrap();
+    let squares: Vec<fabric> = I
+        .lines()
+        .map(|line| {
+            let nums: Vec<i32> = re
+                .captures(line)
+                .unwrap()
+                .iter()
+                .skip(1)
+                .map(|n| i32::from_str(n.unwrap().as_str()).unwrap())
+                .collect();
+
+            fabric {
+                id: nums[0],
+                top_left: [nums[1], nums[2]],
+                bottom_right: [nums[3], nums[4]],
+            }
+        })
+        .collect();
 }
