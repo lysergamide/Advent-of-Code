83805ccd4dac85a7d24fb9ebb4b943753ae76338
eyoson <eyoson@waifu.club>
2021-10-07 15:31:25 -0700

    better

--- 2015/10/10.clj
+++ 2015/10/10.clj
@@ -1,20 +1,13 @@
 (require
   '[clojure.string :refer (split-lines split trim)])
 
-(defn part [numbers]
-  (loop [xs numbers ret '()]
-    (if (empty? xs)
-      (reverse ret)
-      (let [head (first xs)
-            group (take-while #(= head %) xs)
-            leftover (drop-while #(= head %) xs)]
-        (recur leftover (cons group ret))))))
-
 (defn look-and-say [xs]
-  (->> xs
-       part
-       (map #(list (count %) (first %)))
-       flatten))
+  (loop [lst xs ret []]
+    (if (empty? lst)
+      ret
+      (let [head (first lst)
+            [group tail] (split-with (partial = head) lst)]
+        (recur tail (conj ret (count group) head))))))
 
 (defn loop-and-say [xs n]
   (loop [i n ret xs]
@@ -22,14 +15,11 @@
       ret
       (recur (dec i) (look-and-say ret)))))
 
-
 (def I (->> "10.txt" slurp trim seq (map #(Character/digit % 10))))
 
 (def silver-seq (loop-and-say I 40))
 (def silver (count silver-seq))
 (def gold (count (loop-and-say silver-seq 10)))
-
-
 
 (println "Day 10\n======")
 (println (str "✮: " silver))
