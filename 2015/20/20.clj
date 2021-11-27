a1193870a1a83e90085b5f2062b68fcf02fa7c88
eyoson <eyoson@waifu.club>
2021-11-26 19:31:45 -0800

    2015 20 clj done

--- 2015/20/20.clj
+++ 2015/20/20.clj
@@ -1,18 +1,36 @@
 (require '[clojure.string :refer (trim)])
+
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* true)
 
-(def input (->> "20.txt" slurp trim Integer/parseInt))
+(def input
+  (->> "20.txt" slurp trim Integer/parseInt))
 
 (defn silver [x]
   (let [size (quot x 10)
         nums (int-array (repeat size 0))]
-    (loop [i 1 j 1]
-      (cond
-        (>= i size) (first (first (filter (fn [[_ v]] (< x v)) (map-indexed vector nums))))
-        (>= j size) (recur (inc i) (inc i))
-        :else (do (aset nums j (+ (aget nums j) (* 10 i)))
-                  (recur i (+ j i)))))))
 
+    (doseq [i (range 1 size)
+            j (range i size i)]
+      ;; this ^int hint took me an obscene amount to figure out jfc
+      (aset nums j ^int (+ (aget nums j) (* 10 i))))
 
-(println (silver input))
+    (first (first
+            (filter (fn [[_ v]] (< x v))
+                    (map-indexed vector nums))))))
+
+(defn gold [x]
+  (let [size (quot x 10)
+        nums (int-array (repeat size 0))]
+
+    (doseq [i (range 1 size)
+            j (range i (min (inc (* i 50)) size) i)]
+      (aset nums j ^int (+ (aget nums j) (* 11 i))))
+
+    (first (first
+            (filter (fn [[_ v]] (< x v))
+                    (map-indexed vector nums))))))
+
+(println "Day 20\n======")
+(println (str "✮: " (silver input)))
+(println (str "★: " (gold input)))
