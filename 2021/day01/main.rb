e3bb4f536c74e2b0e1f7b250b886ae29d529a45b
eyoson <eyoson@waifu.club>
2021-11-30 22:14:23 -0800

    its that time again

---
+++ 2021/day01/main.rb
@@ -0,0 +1,11 @@
+Nums   = File.read("input").chomp.lines.map(&:to_i)
+Groups = Nums.each_cons(3).map(&:sum)
+
+def solve(xs)
+  xs.each_cons(2).count { _1 < _2 }
+end
+
+puts("Day 01\n"             \
+     "==================\n" \
+     "✮: #{solve(Nums)}\n"   \
+     "★: #{solve(Groups)}")
