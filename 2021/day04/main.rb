87f00fd2774c82021065c5f3f87717998e47a8a4
eyoson <eyoson@waifu.club>
2021-12-04 10:10:21 -0800

    ok with this

--- 2021/day04/main.rb
+++ 2021/day04/main.rb
@@ -1,51 +1,40 @@
+# frozen_string_literal: true
+
 require 'set'
 
-class Board
-  attr_accessor :data
+class Board < Array
+  attr_reader :finish_time
 
-  def initialize(d)
-    @data = d
-    @sets = (d + d.transpose).map(&:to_set)
-    @nums = d.flatten
+  def initialize(arr)
+    super(arr)
+    @finish_time = 0
+    @lots        = []
+    @lines       = (arr + arr.transpose).map(&:to_set)
+    @line_scores = Hash.new(0)
   end
 
-  def bingo?(xs)
-    @sets.any? { |s| xs.count { s.include? _1 } == 5 }
+  def addNum(x)
+    @lots << x
+    @finish_time += 1
+    @lines.select{ _1.include? x }.each{ @line_scores[_1] += 1}
   end
 
-  def score(xs)
-    lel = xs.to_set
-    @nums.filter { !lel.include? _1 }.sum
-  end
+  def bingo? = @line_scores.values.any?{ _1 == self.size }
+  def score  = self.flatten.reject{ @lots.to_set.include? _1 }.sum * @lots[-1]
 end
 
-#I      = File.read("test", chomp: true).split("\n\n")
 I      = File.read("input", chomp: true).split("\n\n")
 Lots   = I.first.split(",").map(&:to_i)
-Boards = I[1..].map { |board| board.lines.map { _1.split.map(&:to_i) } }
-               .map { Board.new _1 } 
 
-winning_board = nil
-winning_num, windex = Lots.each.with_index.find do |n, i|
-  winning_board = Boards.filter { _1.bingo? Lots[..i] }.first
-  !winning_board.nil?
+boards = I[1..].map{ |b| Board.new(b.lines.map{ _1.split.map(&:to_i) }) }
+
+Silver, Gold = Lots.reduce(boards) do |bs, x|
+  break boards.minmax_by(&:finish_time) if bs.empty?
+
+  bs.each{ _1.addNum x}.reject{ _1.bingo? }
 end
 
-p winning_num * winning_board.score(Lots[..windex])
-
-last_board = Boards.dup
-last_num, lastdex = Lots.each.with_index.find do |n, i|
-  last_board = last_board.filter { !_1.bingo? Lots[..i] }
-  if last_board.size == 1 
-    last_board = last_board.first
-    true
-  else
-    false
-  end
-end
-
-last_win = (lastdex..).find do |i|
-  last_board.bingo? Lots[..i]
-end
-
-p Lots[last_win] * last_board.score(Lots[..last_win])
+puts "Day 03\n",
+"==================\n",
+"✮: #{Silver.score}\n",
+"★: #{Gold.score}"
