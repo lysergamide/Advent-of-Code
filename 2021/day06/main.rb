b2e3f4e83a4c361d216f34cb27ae58e3fe008852
eyoson <eyoson@waifu.club>
2021-12-05 23:50:52 -0800

    use array instead of hash

--- 2021/day06/main.rb
+++ 2021/day06/main.rb
@@ -1,22 +1,20 @@
 I  = gets(nil).chomp
               .scan(/\d+/)
               .map(&:to_i)
-              .tally.tap { _1.default = 0 }
+              .reduce(Array.new(9, 0)) { _1[_2] += 1; _1 }
 
 def step(fish)
-  Hash.new(0).tap do |f|
+  Array.new(8, 0).tap do |f|
     (0..7).each{ f[_1] += fish[_1 + 1] }
     f[8]  = fish[0]
     f[6] += fish[0]
   end
 end
 
-sfish = 80.times.reduce(I) { step _1 }
-gfish = (256 - 80).times.reduce(sfish) { step _1 }
-
-Silver, Gold  = [sfish, gfish].map { _1.values.sum } 
+Silver = 80.times.reduce(I) { step _1 }
+Gold   = (256 - 80).times.reduce(Silver) { step _1 }
 
 puts "Day 05\n",
 "==================\n",
-"✮: #{Silver}\n",
-"★: #{Gold}"
+"✮: #{Silver.sum}\n",
+"★: #{Gold.sum}"
