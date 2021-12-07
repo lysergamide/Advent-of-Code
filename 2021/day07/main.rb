f553aaa759efdf9768e944118719bb2c8b3c7e2d
eyoson <eyoson@waifu.club>
2021-12-06 22:33:36 -0800

    lol

--- 2021/day07/main.rb
+++ 2021/day07/main.rb
@@ -1,3 +1,12 @@
-require 'set'
+Nums = $<.read.scan(/\d+/).map(&:to_i).
 
-I = gets(nil).chomp
+def solve(&block)
+  (0..Nums.max).map do |i|
+    Nums.map { block[(i - _1).abs] }.sum
+  end.min
+end
+
+puts "Day 05\n",
+"==================\n",
+"✮: #{solve(&:itself)}\n",
+"★: #{solve{ |x| (x*(x+1)/2) }}"
