f3872abf6d099d06a440fd0e83ed70768ac599d5
eyoson <eyoson@waifu.club>
2021-12-09 23:45:54 -0800

    easy day

--- 2021/day10/main.rb
+++ 2021/day10/main.rb
@@ -1,11 +1,18 @@
 # /user/bin/env ruby
 # frozen_string_literal: true
 
+SScore = { ")" => 3, "]" => 57, "}" => 1197, ">" => 25137 }
+GScore = { "(" => 1, "[" => 2, "{" => 3, "<" => 4 }
 
-Silver = nil
-Gold = nil
+I = $<.readlines(chomp: true).map { while _1.gsub!(/(\(\)|\[\]|{}|<>)/, ""); end; _1 }
 
-puts "Day 10\n",
-"==================\n",
-"✮: #{Silver}\n",
-"★: #{Gold}"
+Silver = I.sum do |line|
+  line.tr("#{GScore.keys.join}", "").chars.first.then{ SScore[_1] || 0 }
+end
+
+Gold = I.map do |line|
+  next nil if Regexp.union(SScore.keys) =~ line
+  line.reverse.chars.reduce(0) { |sum, x| sum * 5 + GScore[x] }
+end.compact.sort.then{ _1[_1.size/2] }
+
+p Silver, Gold
