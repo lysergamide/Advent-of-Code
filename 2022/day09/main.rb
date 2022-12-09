64f0cdc33eb9e09a244949afa69dfb09210770b2
Box of Rain <box@tewi.pw>
2022-12-09 14:12:00 -0800

    formatting

--- 2022/day09/main.rb
+++ 2022/day09/main.rb
@@ -10,9 +10,10 @@ I = $<.readlines(chomp: true).map &:split
 
 DIRS = { 'R' => V[1, 0], 'U' => V[0, 1], 'L' => V[-1, 0], 'D' => V[0, -1] }.freeze
 
-def movement(x0, x1) = (x0 - x1).normalize.map { _1 > 0 ? _1.ceil : _1.floor }
+def movement(x0, x1) =
+  (x0 - x1).normalize.map { _1 > 0 ? _1.ceil : _1.floor }
 
-def solve(size)
+def solve(size) =
   I.reduce([Set[], (0 ... size).map { V[0, 0] }]) do |(seen, xs), (dir, n)|
     n.to_i.times do
       xs[0] += DIRS[dir]
@@ -30,6 +31,5 @@ def solve(size)
   end
   .first
   .size
-end
 
 puts Parallel.map([2, 10]) { solve _1 }
